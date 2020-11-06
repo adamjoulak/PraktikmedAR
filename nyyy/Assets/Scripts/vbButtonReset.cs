314882c62f22e3d1709274670c1b04e458ac4002
adamjoulak <adam.joulak@live.se>
2020-11-06 01:17:14 +0100

    lite ändringar med script
    changes, funkar inte riktigt än

---
+++ nyyy/Assets/Scripts/vbButtonReset.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using Vuforia;
+
+public class vbButtonReset : MonoBehaviour
+{
+    
+    public GameObject vbBtnObjReset;
+    public TextMeshPro tmm;
+    public GameObject topPanel;
+    private bool isReset;
+
+    // Use this for initialization
+    void Start()
+    {
+        topPanel = GameObject.FindGameObjectWithTag("PanelFindTargetTMP");
+        vbBtnObjReset = GameObject.FindGameObjectWithTag("vbBtnObjReset");
+        Debug.Log("found reset button");
+        vbBtnObjReset.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);
+        vbBtnObjReset.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(OnButtonReleased);
+
+    }
+
+    public void SetPanelActive()
+    {
+        topPanel.SetActive(true);
+    }
+    /*
+    public void SetPanelInactive()
+    {
+        topPanel.SetActive(false);
+    }
+    */
+
+    public void SetPanelText(bool isReset)
+    {
+        tmm = (TextMeshPro)GameObject.FindGameObjectWithTag("FindTargetTMP").GetComponent<TextMeshPro>();
+        if (isReset)
+        {
+            tmm.text = "Hitta Alla Bestick & Ingredienser";
+        }
+        else
+        {
+            tmm.text = "Tryck 'Done' Efter Avklarat Delmoment";
+
+        }
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+
+        SetPanelActive();
+        SetPanelText(true);
+        Debug.Log("Top text found Found");
+        Debug.Log("Button pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+
+        Debug.Log("Button released");
+    }
+}
